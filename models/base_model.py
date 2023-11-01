ba9f1ec8e98ad75d2cc60c555106e6c725fdfc60
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-01 19:01:11 -0400

    updating to see if intranet lieks it

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """BaseModel Module"""
 import uuid
 from datetime import datetime
-import models  
+import models
 
 class BaseModel:
     """BaseModels class"""
@@ -11,21 +11,24 @@ class BaseModel:
         """Initialize a new BaseModel."""
         if kwargs:
             for key, value in kwargs.items():
-                # Skip the '__class__' attribute if present, as it's not used for initialization.
                 if key == "__class__":
                     pass
-                # Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
                 elif key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                # Set other attributes from kwargs.
                 else:
                     setattr(self, key, value)
         else:
-            # If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'.
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+
+        # Ensure "id," "created_at," and "updated_at" are explicitly initialized.
+        self.id = kwargs.get("id", self.id)
+        self.created_at = kwargs.get("created_at", self.created_at)
+        self.updated_at = kwargs.get("updated_at", self.updated_at)
+
+        # Notify the storage system of the new instance.
+        models.storage.new(self)
 
     def __str__(self):
         """Return string representation"""
