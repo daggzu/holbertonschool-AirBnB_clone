c273c8affeca8b37a4246292d897c68ddfbcef31
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-01 17:38:45 -0400

    documentation changes + added the models module and used it on the save method

--- models/base_model.py
+++ models/base_model.py
@@ -2,27 +2,27 @@
 """BaseModel Module"""
 import uuid
 from datetime import datetime
+import models  
 
 class BaseModel:
     """BaseModels class"""
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
-
-        # Check if keyword arguments (kwargs) were provided.
+        """Check if keyword arguments (kwargs) were provided."""
         if kwargs:
             for key, value in kwargs.items():
-                # Skip the '__class__' attribute if present, as it's not used for initialization.
+                """Skip the '__class__' attribute if present, as it's not used for initialization."""
                 if key == "__class__":
                     pass
-                # Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
+                """Parse and set 'created_at' and 'updated_at' attributes as datetime objects."""
                 elif key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                # Set other attributes from kwargs.
+                """Set other attributes from kwargs."""
                 else:
                     setattr(self, key, value)
         else:
-            # If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'.
+            """If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'."""
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -32,8 +32,9 @@ class BaseModel:
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Update updated_at with current time"""
+        """Update updated_at with current time and save to storage."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Convert to dictionary"""
