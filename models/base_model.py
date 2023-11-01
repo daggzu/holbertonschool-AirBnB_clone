2021ecd3682d87d2a52d0aaf87a438c67bfae83e
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-01 12:10:39 -0400

    changing indentation

--- models/base_model.py
+++ models/base_model.py
@@ -7,29 +7,29 @@ import models
 class BaseModel:
     """BaseModels class"""
 
-def __init__(self, *args, **kwargs):
-    """Initialize a new BaseModel."""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel."""
 
-    # Check if keyword arguments (kwargs) were provided.
-    if kwargs:
-        for key, value in kwargs.items():
-            # Skip the '__class__' attribute if present, as it's not used for initialization.
-            if key == "__class__":
-                pass
-            # Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
-            elif key == "created_at" or key == "updated_at":
-                setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-            # Set other attributes from kwargs.
-            else:
-                setattr(self, key, value)
-    else:
-        # If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'.
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        # Check if keyword arguments (kwargs) were provided.
+        if kwargs:
+            for key, value in kwargs.items():
+                # Skip the '__class__' attribute if present, as it's not used for initialization.
+                if key == "__class__":
+                    pass
+                # Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
+                elif key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                # Set other attributes from kwargs.
+                else:
+                    setattr(self, key, value)
+        else:
+            # If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'.
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
-        # Notify the storage system of the new instance.
-        models.storage.new(self)
+            # Notify the storage system of the new instance.
+            models.storage.new(self)
 
     def __str__(self):
         """Return string representation"""
