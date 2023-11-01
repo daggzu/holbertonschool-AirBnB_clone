5bf33b06c2cfe4a9d71b5485d2cb092b841aa43e
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-01 12:26:30 -0400

    indentation

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
             # Notify the storage system of the new instance.
             models.storage.new(self)
 
