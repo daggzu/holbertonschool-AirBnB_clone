db8c70f7b0bd5032c2b51af96609a380f567fe84
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-01 17:41:46 -0400

    fixing comments once again

--- models/base_model.py
+++ models/base_model.py
@@ -9,20 +9,19 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize a new BaseModel."""
-        #Check if keyword arguments (kwargs) were provided.
         if kwargs:
             for key, value in kwargs.items():
-                """Skip the '__class__' attribute if present, as it's not used for initialization."""
+                # Skip the '__class__' attribute if present, as it's not used for initialization.
                 if key == "__class__":
                     pass
-                #Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
+                # Parse and set 'created_at' and 'updated_at' attributes as datetime objects.
                 elif key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
-                """Set other attributes from kwargs."""
+                # Set other attributes from kwargs.
                 else:
                     setattr(self, key, value)
         else:
-            """If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'."""
+            # If no kwargs are provided, initialize default values for 'id', 'created_at', and 'updated_at'.
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
