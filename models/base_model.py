0e180b0790527bd402e83fd8e2c2e4c43c0172d0
daggzu <112585566+daggzu@users.noreply.github.com>
2023-11-02 10:25:48 -0400

    pycodestyle

--- models/base_model.py
+++ models/base_model.py
@@ -46,3 +46,4 @@ class BaseModel:
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         return dictionary
+        
